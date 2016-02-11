674a2f38967a55c982004f1670076cbcbca8da51
poudigne <pierluc0414@live.ca>
2016-02-11 00:14:07 -0500

    End of chapter 4

---
+++ Assets/Scripts/Editor/NewTileMap.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class NewTileMap : EditorWindow {
+
+
+    [MenuItem("GameObject/Create Other/Tile Map")]
+	public static void Init()
+    {
+        GameObject go = new GameObject("Tile map");
+        go.AddComponent<TileMap>();
+    }
+}
