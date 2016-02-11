31db65f0ad592dba631c8385ac7818af539cfdaa
poudigne <pierluc0414@live.ca>
2016-02-11 00:55:35 -0500

    done.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (target != null)
+        {
+            var t = target.transform;
+            transform.position = new Vector3(t.position.x, t.position.y, transform.position.z);
+        }
+	}
+}
